94206b44551d8150f16adf2c5c895388bd235350
Kacper Majcher <majchermajus@proton.me>
2024-07-09 12:09:04 +0200

    feat: support for streamtape host

--- lib/types/film_details.dart
+++ lib/types/film_details.dart
@@ -39,12 +39,13 @@ class DirectLink {
   final String link;
   final String qualityVersion;
   final String language;
+  final String hostingImgUrl;
 
-  DirectLink({
-    required this.link,
-    required this.qualityVersion,
-    required this.language,
-  });
+  DirectLink(
+      {required this.link,
+      required this.qualityVersion,
+      required this.language,
+      required this.hostingImgUrl});
 }
 
 class FilmDetails {
@@ -68,12 +69,19 @@ class FilmDetails {
   Future<List<DirectLink>> getDirect() async {
     List<DirectLink> directLinks = [];
     for (Link link in links ?? []) {
+      if (link.main.toString().contains("streamtape")) {
+        directLinks.add(DirectLink(
+            link: await scrapStreamtape(link.link),
+            qualityVersion: link.qualityVersion,
+            language: link.language,
+            hostingImgUrl: link.hostingImgUrl));
+      }
       if (link.main.toString().contains("vidoza")) {
         directLinks.add(DirectLink(
-          link: await scrapVidoza(link.link),
-          qualityVersion: link.qualityVersion,
-          language: link.language,
-        ));
+            link: await scrapVidoza(link.link),
+            qualityVersion: link.qualityVersion,
+            language: link.language,
+            hostingImgUrl: link.hostingImgUrl));
       }
     }
     return directLinks;
@@ -81,12 +89,67 @@ class FilmDetails {
 
   Future<String> scrapVidoza(String url) async {
     final Dio dio = Dio();
-    final response = await dio.get(url);
-    final document = parse(response.data);
+    try {
+      final response = await dio.get(url);
+      final document = parse(response.data);
 
-    final link = document.querySelector('source')?.attributes['src'];
+      final link = document.querySelector('source')?.attributes['src'];
 
-    return Uri.parse(link ?? '').toString();
+      return Uri.parse(link ?? '').toString();
+    } catch (e) {
+      throw Exception('Error scraping Vidoza.net: ${e.toString()}');
+    }
+  }
+
+  Future<String> scrapStreamtape(String url) async {
+    final Dio dio = Dio();
+
+    try {
+      final response = await dio.get(url);
+
+      final jsLineMatch = RegExp(
+              r"(?<=document\.getElementById\('botlink'\)\.innerHTML = )(.*)(?=;)")
+          .firstMatch(response.data);
+
+      if (jsLineMatch == null || jsLineMatch.group(0) == null) {
+        throw Exception('Error scraping Streamtape.com: No JS line found');
+      }
+
+      final String jsLine = jsLineMatch.group(0)!;
+
+      final List<String> urls = RegExp(r"'([^']*)'")
+          .allMatches(jsLine)
+          .map((m) => m.group(0)!.replaceAll("'", ""))
+          .toList();
+
+      if (urls.length != 2) {
+        throw Exception('Error scraping Streamtape.com: No URL in JS line');
+      }
+
+      final String base = urls[0];
+      final String encoded = urls[1];
+
+      final String fullUrl = 'https:$base${encoded.substring(4)}';
+
+      final apiResponse = await dio.get(
+        fullUrl,
+        options: Options(
+          followRedirects: false,
+          validateStatus: (status) =>
+              status != null && status >= 200 && status < 400,
+        ),
+      );
+
+      final String? directLink = apiResponse.headers["location"]?.first;
+
+      if (directLink == null) {
+        throw Exception('Error scraping Streamtape.com: No direct link found');
+      }
+
+      return directLink;
+    } catch (e) {
+      throw Exception('Error scraping Streamtape.com: ${e.toString()}');
+    }
   }
 
   List<Season> getSeasons() {
