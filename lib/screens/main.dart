5fb58d9595e5553c30ce73915b293f64f1bbc560
TreeKiller <99129304+treekiller12@users.noreply.github.com>
2024-12-01 22:30:18 +0100

    Update main.dart

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,13 +1,14 @@
-import "package:flutter/material.dart";
-import "package:flutter/services.dart";
-import "package:provider/provider.dart";
-import "package:unofficial_filman_client/notifiers/filman.dart";
-import "package:unofficial_filman_client/screens/category.dart";
-import "package:unofficial_filman_client/screens/hello.dart";
-import "package:unofficial_filman_client/screens/main/home.dart";
-import "package:unofficial_filman_client/screens/main/offline.dart";
-import "package:unofficial_filman_client/screens/main/watched.dart";
-import "package:unofficial_filman_client/screens/settings.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:unofficial_filman_client/notifiers/filman.dart';
+import 'package:unofficial_filman_client/screens/category.dart';
+import 'package:unofficial_filman_client/screens/hello.dart';
+import 'package:unofficial_filman_client/screens/main/home.dart';
+import 'package:unofficial_filman_client/screens/main/offline.dart';
+import 'package:unofficial_filman_client/screens/main/watched.dart';
+import 'package:unofficial_filman_client/screens/settings.dart';
+import 'package:unofficial_filman_client/widgets/search.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -19,87 +20,62 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int currentPageIndex = 0;
   int? focusedIndex;
+  int? clickedIndex;
 
   @override
   Widget build(final BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Row(
+        child: Column(
           children: [
             Container(
-              width: 250,
-              color: Colors.grey[900],
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  _buildNavItem(
-                    icon: Icons.home,
-                    label: "Strona Główna",
-                    index: 0,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Image.asset(
+                      'assets/images/flutter_logo.png',
+                      width: 40,
+                      height: 40,
+                    ),
                   ),
-                  _buildNavItem(
-                    icon: Icons.watch_later,
-                    label: "Oglądane",
-                    index: 1,
+                  _buildSearchButton(),
+                  SizedBox(width: 8),
+                  Row(
+                    children: [
+                      _buildNavItem(index: 0, label: "Strona Główna"),
+                      _buildCategoryButton(),
+                      _buildNavItem(index: 1, label: "Oglądane"),
+                      _buildNavItem(index: 2, label: "Pobrane"),
+                    ],
                   ),
+                  Spacer(),
                   _buildNavItem(
-                    icon: Icons.download,
-                    label: "Pobrane",
-                    index: 2,
-                  ),
-                  _buildNavItem(
-                    icon: Icons.category,
-                    label: "Filmy",
-                    index: 3,
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (final context) => const CategoryScreen(
-                            forSeries: false,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  _buildNavItem(
-                    icon: Icons.category,
-                    label: "Seriale",
-                    index: 4,
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (final context) => const CategoryScreen(
-                            forSeries: true,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  _buildNavItem(
-                    icon: Icons.settings,
-                    label: "Ustawienia",
                     index: 5,
+                    icon: Icons.settings,
                     onTap: () {
                       Navigator.of(context).push(
                         MaterialPageRoute(
-                          builder: (final context) => const SettingsScreen(),
+                          builder: (context) => const SettingsScreen(),
                         ),
                       );
                     },
                   ),
                   _buildNavItem(
-                      icon: Icons.logout,
-                      label: "Wyloguj się",
-                      index: 6,
-                      onTap: () {
-                        Provider.of<FilmanNotifier>(context, listen: false)
-                            .logout();
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (final context) => const HelloScreen(),
-                          ),
-                        );
-                      })
+                    index: 6,
+                    icon: Icons.logout,
+                    onTap: () {
+                      Provider.of<FilmanNotifier>(context, listen: false)
+                          .logout();
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (final context) => const HelloScreen(),
+                        ),
+                      );
+                    },
+                  ),
                 ],
               ),
             ),
@@ -120,22 +96,21 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   Widget _buildNavItem({
-    required final IconData icon,
-    required final String label,
-    required final int index,
+    final int? index,
+    final String? label,
+    final IconData? icon,
     final VoidCallback? onTap,
   }) {
     final bool isSelected = currentPageIndex == index;
     final bool isFocused = focusedIndex == index;
 
     return Focus(
-      autofocus: index == 0,
-      onFocusChange: (final hasFocus) {
+      onFocusChange: (hasFocus) {
         setState(() {
           focusedIndex = hasFocus ? index : null;
         });
       },
-      onKeyEvent: (final FocusNode node, final KeyEvent event) {
+      onKeyEvent: (FocusNode node, KeyEvent event) {
         if (event is KeyDownEvent) {
           if (event.logicalKey == LogicalKeyboardKey.select ||
               event.logicalKey == LogicalKeyboardKey.enter) {
@@ -143,7 +118,8 @@ class _MainScreenState extends State<MainScreen> {
               onTap();
             } else {
               setState(() {
-                currentPageIndex = index;
+                currentPageIndex = index!;
+                clickedIndex = index;
               });
             }
             return KeyEventResult.handled;
@@ -157,34 +133,202 @@ class _MainScreenState extends State<MainScreen> {
             onTap();
           } else {
             setState(() {
-              currentPageIndex = index;
+              currentPageIndex = index!;
+              clickedIndex = null;
             });
           }
         },
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 8),
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          margin: const EdgeInsets.symmetric(horizontal: 4),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
           decoration: BoxDecoration(
-            color: isFocused ? Colors.grey[800] : Colors.transparent,
-            borderRadius:
-                const BorderRadius.horizontal(right: Radius.circular(16)),
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(16),
           ),
-          child: Row(
+          child: Transform.scale(
+            scale: isFocused || clickedIndex == index ? 1.1 : 1.0,
+            child: Column(
+              children: [
+                if (icon != null)
+                  Icon(
+                    icon,
+                    size: isFocused || clickedIndex == index ? 26 : 24,
+                    color: isSelected || isFocused || clickedIndex == index
+                        ? Colors.white
+                        : Colors.grey,
+                  ),
+                if (label != null)
+                  AnimatedDefaultTextStyle(
+                    style: TextStyle(
+                      fontSize: isFocused || clickedIndex == index ? 15 : 14,
+                      fontWeight: FontWeight.bold,
+                      color: isSelected || isFocused || clickedIndex == index
+                          ? Colors.white
+                          : Colors.grey,
+                    ),
+                    duration: const Duration(milliseconds: 300),
+                    child: Text(label),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showCategoryOptions(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Wybierz typ'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Icon(
-                icon,
-                size: 36,
-                color: isSelected || isFocused ? Colors.white : Colors.grey,
+              ListTile(
+                title: const Text('Filmy'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const CategoryScreen(forSeries: false),
+                    ),
+                  );
+                },
               ),
-              const SizedBox(width: 16),
-              Text(
-                label,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: isSelected || isFocused ? Colors.white : Colors.grey,
-                ),
+              ListTile(
+                title: const Text('Seriale'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const CategoryScreen(forSeries: true),
+                    ),
+                  );
+                },
               ),
             ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildSearchButton() {
+    final bool isFocused = focusedIndex == 200;
+
+    return Focus(
+      onFocusChange: (hasFocus) {
+        setState(() {
+          focusedIndex = hasFocus ? 200 : null;
+        });
+      },
+      onKeyEvent: (FocusNode node, KeyEvent event) {
+        if (event is KeyDownEvent) {
+          if (event.logicalKey == LogicalKeyboardKey.enter ||
+              event.logicalKey == LogicalKeyboardKey.select) {
+            _showBottomSheet();
+            return KeyEventResult.handled;
+          }
+        }
+        return KeyEventResult.ignored;
+      },
+      child: GestureDetector(
+        onTap: _showBottomSheet,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          margin: const EdgeInsets.symmetric(horizontal: 8),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: AnimatedScale(
+            scale: isFocused || clickedIndex == 200 ? 1.1 : 1.0,
+            duration: const Duration(milliseconds: 300),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  size: 26,
+                  color: isFocused || clickedIndex == 200 ? Colors.white : Colors.grey,
+                ),
+                const SizedBox(width: 8),
+                AnimatedDefaultTextStyle(
+                  duration: const Duration(milliseconds: 300),
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: isFocused || clickedIndex == 200 ? Colors.white : Colors.grey,
+                  ),
+                  child: const Text("Szukaj"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => const SearchModal(),
+    );
+  }
+
+  Widget _buildCategoryButton() {
+    return Focus(
+      onFocusChange: (hasFocus) {
+        setState(() {
+          focusedIndex = hasFocus ? 3 : null;
+        });
+      },
+      onKeyEvent: (FocusNode node, KeyEvent event) {
+        if (event is KeyDownEvent) {
+          if (event.logicalKey == LogicalKeyboardKey.enter ||
+              event.logicalKey == LogicalKeyboardKey.select) {
+            _showCategoryOptions(context);
+            return KeyEventResult.handled;
+          }
+        }
+        return KeyEventResult.ignored;
+      },
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            clickedIndex = 3;
+          });
+          _showCategoryOptions(context);
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          margin: const EdgeInsets.symmetric(horizontal: 4),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: AnimatedScale(
+            scale: focusedIndex == 3 || clickedIndex == 3 ? 1.1 : 1.0,
+            duration: const Duration(milliseconds: 300),
+            child: AnimatedDefaultTextStyle(
+              duration: const Duration(milliseconds: 300),
+              style: TextStyle(
+                fontSize: focusedIndex == 3 || clickedIndex == 3 ? 15 : 14,
+                fontWeight: FontWeight.bold,
+                color: focusedIndex == 3 || clickedIndex == 3
+                    ? Colors.white
+                    : Colors.grey,
+              ),
+              child: const Text("Kategorie"),
+            ),
           ),
         ),
       ),
