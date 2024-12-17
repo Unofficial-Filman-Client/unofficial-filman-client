49ea706ba16b899bb4e4716dd64319c4aad2c8d4
TreeKiller <99129304+treekiller12@users.noreply.github.com>
2024-12-17 22:32:23 +0100

    Update watched.dart

--- lib/screens/main/watched.dart
+++ lib/screens/main/watched.dart
@@ -1,200 +1,311 @@
-import "package:fast_cached_network_image/fast_cached_network_image.dart";
-import "package:unofficial_filman_client/notifiers/settings.dart";
+// ignore_for_file: deprecated_member_use
+
 import "package:unofficial_filman_client/notifiers/watched.dart";
 import "package:unofficial_filman_client/screens/film.dart";
 import "package:unofficial_filman_client/screens/player.dart";
 import "package:unofficial_filman_client/types/watched.dart";
-import "package:unofficial_filman_client/utils/title.dart";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:provider/provider.dart";
 
 class WatchedPage extends StatefulWidget {
-  const WatchedPage({super.key});
-
+  final Function(bool) onHoverStateChanged;
+  
+  const WatchedPage({
+    super.key, 
+    required this.onHoverStateChanged
+  });
+  
   @override
   State<WatchedPage> createState() => _WatchedPageState();
 }
 
 class _WatchedPageState extends State<WatchedPage> {
-  Widget _buildWatchedFilmCard(final BuildContext context,
-      final WatchedSingle film, final WatchedNotifier all) {
-    return Card(
-      child: Stack(
-        children: [
-          InkWell(
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (final context) => FilmanPlayer.fromDetails(
-                    filmDetails: film.filmDetails,
-                    startFrom: film.watchedInSec,
-                    savedDuration: film.totalInSec,
+  final List<FocusNode> _focusNodes = [];
+  final ScrollController _scrollController = ScrollController();
+  final FocusNode _emptyStateFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 100; i++) {
+      _focusNodes.add(FocusNode());
+    }
+  }
+
+  @override
+void dispose() {
+  for (var node in _focusNodes) {
+    node.dispose();
+  }
+  _scrollController.dispose();
+  _emptyStateFocusNode.dispose();
+  super.dispose();
+}
+
+  void _showFilmMenu(final BuildContext context, final WatchedSingle film) {
+    showDialog(
+      context: context,
+      builder: (final context) {
+        return SimpleDialog(
+          title: const Center(child: Text("Opcje dla filmu")),
+          children: [
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (final context) => FilmanPlayer.fromDetails(
+                      filmDetails: film.filmDetails,
+                      startFrom: film.watchedInSec,
+                      savedDuration: film.totalInSec,
+                    ),
+                  ),
+                ).then((final _) {
+                  setState(() {});
+                });
+              },
+              child: const Center(child: Text("Kontynuuj")),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (final context) => FilmScreen.fromDetails(details: film.filmDetails),
+                  ),
+                ).then((final _) {
+                  setState(() {});
+                });
+              },
+              child: const Center(child: Text("Informacje")),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _showDeleteConfirmationDialog(context, film);
+              },
+              child: const Center(child: Text("Usuń")),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDeleteConfirmationDialog(final BuildContext context, final WatchedSingle film) {
+    showDialog(
+      context: context,
+      builder: (final context) {
+        return AlertDialog(
+          title: const Text("Potwierdzenie usunięcia"),
+          content: Text("Czy na pewno chcesz usunąć \"${film.filmDetails.title}\"?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("Anuluj"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  Provider.of<WatchedNotifier>(context, listen: false)
+                      .remove(film);
+                });
+              },
+              child: const Text("Usuń"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildWatchedFilmCard(
+  final BuildContext context, 
+  final WatchedSingle film, 
+  final int index,
+  {final bool isFirstItem = false}
+) {
+  return Focus(
+    focusNode: _focusNodes[index],
+    onKey: (final node, final event) {
+      if (event is RawKeyDownEvent) {
+        if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+          if (index + 3 < _focusNodes.length) {
+            _focusNodes[index + 3].requestFocus();
+            _scrollToVisible(index + 3);
+            return KeyEventResult.handled;
+          }
+        } else if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+          if (index < 3) {
+            widget.onHoverStateChanged(false);
+            return KeyEventResult.handled;
+          } else if (index - 3 >= 0) {
+            _focusNodes[index - 3].requestFocus();
+            _scrollToVisible(index - 3);
+            return KeyEventResult.handled;
+          }
+        } else if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+          if (index - 1 >= 0 && index % 3 != 0) {
+            _focusNodes[index - 1].requestFocus();
+            return KeyEventResult.handled;
+          }
+        } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+          if (index + 1 < _focusNodes.length && (index + 1) % 3 != 0) {
+            _focusNodes[index + 1].requestFocus();
+            return KeyEventResult.handled;
+          }
+        } else if (event.logicalKey == LogicalKeyboardKey.select ||
+                   event.logicalKey == LogicalKeyboardKey.enter) {
+          _showFilmMenu(context, film);
+          return KeyEventResult.handled;
+        }
+      }
+      return KeyEventResult.ignored;
+    },
+    onFocusChange: (final hasFocus) {
+      setState(() {});
+      if (isFirstItem) {
+        widget.onHoverStateChanged(hasFocus);
+      }
+    },
+    child: AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: _focusNodes[index].hasFocus ? Colors.blue : Colors.transparent,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          _showFilmMenu(context, film);
+        },
+        child: Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 5,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.vertical(top: Radius.circular(12.0)),
+                  child: Image.network(
+                    film.filmDetails.imageUrl,
+                    fit: BoxFit.cover,
                   ),
                 ),
-              );
-            },
-            onLongPress: () => showDialog(
-              context: context,
-              builder: (final context) {
-                return AlertDialog(
-                  title: const Text("Usuwanie z historii"),
-                  content: Consumer<SettingsNotifier>(
-                    builder: (final context, final settings, final child) =>
-                        Text(
-                      "Czy na pewno chcesz usunąć postęp oglądania \"${getDisplayTitle(film.filmDetails.title, settings)}\" z historii?",
+              ),
+              LinearProgressIndicator(
+                value: film.watchedPercentage,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      film.filmDetails.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
                     ),
-                  ),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text("Anuluj"),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        setState(() {
-                          Provider.of<WatchedNotifier>(context, listen: false)
-                              .remove(film);
-                        });
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text("Usuń"),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      "Pozostało: ${film.totalInSec ~/ 60} min",
+                      textAlign: TextAlign.center,
                     ),
                   ],
-                );
-              },
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: ClipRRect(
-                    borderRadius:
-                        const BorderRadius.vertical(top: Radius.circular(12.0)),
-                    child: FastCachedImage(
-                      url: film.filmDetails.imageUrl,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
                 ),
-                LinearProgressIndicator(
-                  value: film.watchedPercentage,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        if (film.filmDetails.isEpisode)
-                          Column(
-                            children: [
-                              Text(
-                                (film.filmDetails.seasonEpisodeTag?.split(" ")
-                                          ?..removeAt(0))
-                                        ?.join(" ") ??
-                                    "",
-                                maxLines: 1,
-                                overflow: TextOverflow.fade,
-                                textAlign: TextAlign.center,
-                              ),
-                              Text(
-                                'S${film.parentSeason?.seasonTitle.replaceAll('Sezon ', '')}:O${1 + (film.parentSeason?.episodes.indexWhere((final e) => e.episodeUrl == film.filmDetails.url) ?? 0)} z ${film.parentSeason?.episodes.length}',
-                                style: const TextStyle(
-                                  fontSize: 16.0,
-                                ),
-                                maxLines: 1,
-                                overflow: TextOverflow.fade,
-                                textAlign: TextAlign.center,
-                              ),
-                            ],
-                          )
-                        else
-                          Text(
-                            "Pozostało: ${film.totalInSec ~/ 60} min",
-                            maxLines: 1,
-                            overflow: TextOverflow.fade,
-                            textAlign: TextAlign.center,
-                          ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Positioned(
-            right: 0,
-            top: 0,
-            child: film.filmDetails.parentUrl != null
-                ? IconButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (final context) => FilmScreen(
-                            url: film.filmDetails.parentUrl!,
-                            image: film.filmDetails.imageUrl,
-                            title: film.filmDetails.title,
-                          ),
-                        ),
-                      );
-                    },
-                    icon: const Icon(Icons.info),
-                  )
-                : IconButton(
-                    onPressed: () => Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (final context) =>
-                            FilmScreen.fromDetails(details: film.filmDetails),
-                      ),
-                    ),
-                    icon: const Icon(Icons.info),
-                  ),
-          )
-        ],
+        ),
       ),
+    ),
+  );
+}
+
+
+  void _scrollToVisible(final int index) {
+    final double position = (index ~/ 3) * 200.0;
+    _scrollController.animateTo(
+      position,
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeInOut,
     );
   }
 
   @override
-  Widget build(final BuildContext context) {
-    return Consumer<WatchedNotifier>(
-      builder: (final context, final value, final child) {
-        final List<WatchedSingle> combined = value.films +
-            value.serials.map((final e) => e.lastWatched).toList();
-        combined.sort((final a, final b) => b.watchedAt.compareTo(a.watchedAt));
+Widget build(final BuildContext context) {
+  return Consumer<WatchedNotifier>(
+    builder: (final context, final value, final child) {
+      final List<WatchedSingle> combined = value.films +
+          value.serials.map((final e) => e.lastWatched).toList();
+      combined.sort((final a, final b) => b.watchedAt.compareTo(a.watchedAt));
 
-        return combined.isEmpty
-            ? Center(
-                child: Text(
+      if (combined.isEmpty) {
+  return Focus(
+    focusNode: _emptyStateFocusNode,
+    onFocusChange: (final hasFocus) {
+      widget.onHoverStateChanged(hasFocus);
+    },
+    child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.history, size: 50, color: Colors.grey),
+                Text(
                   "Brak filmów w historii oglądania",
-                  style: Theme.of(context).textTheme.labelLarge,
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelLarge
+                      ?.copyWith(color: Colors.grey),
                 ),
-              )
-            : GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 6,
-                  mainAxisSpacing: 10,
-                  mainAxisExtent: 250,
-                ),
-                padding: const EdgeInsets.all(10),
-                itemCount: combined.length,
-                itemBuilder: (final BuildContext context, final int index) {
-                  final film = combined[index];
-                  return Align(
-                    alignment: Alignment.topCenter,
-                    child: SizedBox(
-                      height: 250,
-                      child: _buildWatchedFilmCard(context, film, value),
-                    ),
-                  );
-                },
-              );
-      },
+              ],
+            ),
+          ),
+        );
+      }
+
+      return GridView.builder(
+        controller: _scrollController,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 6,
+          mainAxisSpacing: 10,
+          mainAxisExtent: 200.0,
+        ),
+        padding: const EdgeInsets.all(10),
+        itemCount: combined.length,
+        itemBuilder: (final BuildContext context, final int index) {
+    final film = combined[index];
+    return Align(
+      alignment: Alignment.topCenter,
+      child: SizedBox(
+        height: 200,
+        child: _buildWatchedFilmCard(
+          context, 
+          film, 
+          index,
+          isFirstItem: index == 0,
+        ),
+      ),
     );
-  }
+  },
+);
+},
+);
+}
 }
