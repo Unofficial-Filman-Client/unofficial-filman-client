b2a476be5a8022817af56dbfd5f4a0723bb550cb
Kacper Majcher <majchermajus@proton.me>
2025-05-31 22:10:12 +0200

    feat: purevideo

--- lib/screens/player.dart
+++ lib/screens/player.dart
@@ -2,8 +2,6 @@ import "dart:async";
 import "dart:math";
 import "dart:io" show Directory;
 
-import "package:flutter_cast_framework/cast.dart";
-import "package:flutter_cast_framework/widgets.dart";
 import "package:unofficial_filman_client/notifiers/filman.dart";
 import "package:unofficial_filman_client/notifiers/settings.dart";
 import "package:unofficial_filman_client/notifiers/watched.dart";
@@ -29,40 +27,36 @@ class FilmanPlayer extends StatefulWidget {
   final FilmDetails? parentDetails;
   final int startFrom;
   final int savedDuration;
-  final FlutterCastFramework? castFramework;
   final DownloadedSingle? downloaded;
   final DownloadedSerial? parentDownloaded;
 
-  const FilmanPlayer(
-      {super.key,
-      required this.targetUrl,
-      this.parentDetails,
-      this.startFrom = 0,
-      this.savedDuration = 0,
-      this.castFramework})
-      : filmDetails = null,
+  const FilmanPlayer({
+    super.key,
+    required this.targetUrl,
+    this.parentDetails,
+    this.startFrom = 0,
+    this.savedDuration = 0,
+  })  : filmDetails = null,
         downloaded = null,
         parentDownloaded = null;
 
-  const FilmanPlayer.fromDetails(
-      {super.key,
-      required this.filmDetails,
-      this.parentDetails,
-      this.startFrom = 0,
-      this.savedDuration = 0,
-      this.castFramework})
-      : targetUrl = "",
+  const FilmanPlayer.fromDetails({
+    super.key,
+    required this.filmDetails,
+    this.parentDetails,
+    this.startFrom = 0,
+    this.savedDuration = 0,
+  })  : targetUrl = "",
         downloaded = null,
         parentDownloaded = null;
 
-  FilmanPlayer.fromDownload(
-      {super.key,
-      required this.downloaded,
-      this.parentDownloaded,
-      this.startFrom = 0,
-      this.savedDuration = 0,
-      this.castFramework})
-      : targetUrl = "",
+  FilmanPlayer.fromDownload({
+    super.key,
+    required this.downloaded,
+    this.parentDownloaded,
+    this.startFrom = 0,
+    this.savedDuration = 0,
+  })  : targetUrl = "",
         filmDetails = downloaded?.film,
         parentDetails = parentDownloaded?.serial;
 
@@ -96,9 +90,7 @@ class _FilmanPlayerState extends State<FilmanPlayer> {
   DownloadedSingle? _nextDwonloaded;
   String _displayState = "Ładowanie...";
 
-  late FlutterCastFramework _castFramework;
   String? _direct;
-  CastState? _castState;
 
   @override
   void initState() {
@@ -111,41 +103,7 @@ class _FilmanPlayerState extends State<FilmanPlayer> {
     _initMediaKit();
     _initSubscriptions();
     _initPlayer();
-    _initCast();
     super.initState();
-  }
-
-  void _initCast() {
-    if (widget.castFramework != null) {
-      _castFramework = widget.castFramework!;
-    } else {
-      _castFramework = FlutterCastFramework.create([
-        "urn:x-cast:majusss-unofficial-filman-client",
-      ]);
-    }
-    _castFramework.castContext.state.addListener(
-      () async {
-        setState(() {
-          _castState = _castFramework.castContext.state.value;
-        });
-        switch (_castFramework.castContext.state.value) {
-          case CastState.connected:
-            _castVideo();
-            break;
-          case CastState.idle:
-          default:
-            break;
-        }
-      },
-    );
-
-    final sessionManager = _castFramework.castContext.sessionManager;
-    sessionManager.remoteMediaClient.onProgressUpdated =
-        (final progressMs, final durationMs) {
-      setState(() {
-        _position = Duration(milliseconds: progressMs);
-      });
-    };
   }
 
   void _initMediaKit() {
@@ -322,32 +280,6 @@ class _FilmanPlayerState extends State<FilmanPlayer> {
     super.dispose();
   }
 
-  void _castVideo() async {
-    _player.pause();
-    _castFramework.castContext.sessionManager.remoteMediaClient.load(
-        MediaLoadRequestData(
-            currentTime: _position.inMilliseconds,
-            shouldAutoplay: true,
-            mediaInfo: MediaInfo(
-                streamDuration: _duration.inMilliseconds,
-                streamType: StreamType.buffered,
-                contentType: "videos/mp4",
-                contentId: _direct,
-                mediaMetadata: MediaMetadata(
-                    mediaType: MediaType.movie,
-                    strings: _filmDetails.isEpisode
-                        ? {
-                            MediaMetadataKey.title.name:
-                                _filmDetails.seasonEpisodeTag,
-                            MediaMetadataKey.subtitle.name: _filmDetails.title,
-                          }
-                        : {MediaMetadataKey.title.name: _filmDetails.title},
-                    webImages: [
-                      WebImage(url: _filmDetails.imageUrl),
-                      WebImage(url: _filmDetails.imageUrl)
-                    ]))));
-  }
-
   @override
   Widget build(final BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
@@ -381,7 +313,6 @@ class _FilmanPlayerState extends State<FilmanPlayer> {
                   children: [
                     _buildTopBar(),
                     _buildCenterPlayPauseButton(),
-                    _buildIconsBar(),
                     _buildBrightnessControl(),
                     _buildBottomBar(),
                   ],
@@ -390,25 +321,6 @@ class _FilmanPlayerState extends State<FilmanPlayer> {
         )
       ],
     );
-  }
-
-  Widget _buildIconsBar() {
-    return Positioned(
-        height: MediaQuery.of(context).size.height,
-        right: 10,
-        top: -10,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _castState != CastState.unavailable
-                ? CastButton(
-                    castFramework: _castFramework,
-                    color: Colors.white,
-                    activeColor: Theme.of(context).colorScheme.primary,
-                  )
-                : const SizedBox()
-          ],
-        ));
   }
 
   Widget _buildLoadingIcon() {
@@ -618,14 +530,13 @@ class _FilmanPlayerState extends State<FilmanPlayer> {
                             MaterialPageRoute(builder: (final context) {
                           if (_nextEpisode != null) {
                             return FilmanPlayer.fromDetails(
-                                filmDetails: _nextEpisode,
-                                castFramework: _castFramework);
+                              filmDetails: _nextEpisode,
+                            );
                           }
                           if (_nextDwonloaded != null) {
                             return FilmanPlayer.fromDownload(
                               downloaded: _nextDwonloaded,
                               parentDownloaded: widget.parentDownloaded,
-                              castFramework: _castFramework,
                             );
                           }
                           return const Center(child: Text("Wystąpił błąd"));
